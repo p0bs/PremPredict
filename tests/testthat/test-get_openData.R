538593fb50b4967441f4de54c35c48479e4f1592
p0bs <robin_penfold@yahoo.com>
2025-05-09 20:54:32 +0100

    Add external data for 23-24 season, for use in calculations and tests. Alter the get_openData test accordingly

--- tests/testthat/test-get_openData.R
+++ tests/testthat/test-get_openData.R
@@ -1,10 +1,16 @@
 data_download <- get_openData(
-  value_path = "https://raw.githubusercontent.com/openfootball/football.json/refs/heads/master/2024-25/en.1.json",
+  value_path = system.file(
+    "extdata",
+    "en1_2324.json",
+    package = "PremPredict",
+    mustWork = TRUE
+    ),
   table_teams = data("teams"),
   value_yearEnd = 2025L
   )
 
 test_that("correct size", {
-  expect_equal(nrow(data_download), 380)
+  # Without promotion/relegation: 380; with: 272
+  expect_equal(nrow(data_download), 272)
   expect_equal(ncol(data_download), 10)
 })
