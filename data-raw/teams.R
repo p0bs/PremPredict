5b16ee11e1a90034de4a2821441ffa798d3696c1
p0bs <robin_penfold@yahoo.com>
2025-10-23 11:26:49 +0100

    Add a new way to get the schedule data for a season, so that I can use another source to get the results in a more timely manner

--- data-raw/teams.R
+++ data-raw/teams.R
@@ -3,27 +3,27 @@
 #  and that all other names are unique
 
 teams <- tibble::tribble(
-  ~teamName, ~shortName, ~midName,
-  "Arsenal FC", "ARS", "Arsenal",
-  "Aston Villa FC", "AST", "Aston Villa",
-  "AFC Bournemouth", "BOU", "Bournemouth",
-  "Brentford FC", "BRE", "Brentford",
-  "Brighton & Hove Albion FC", "BRI", "Brighton",
-  "Burnley FC", "BUR", "Burnley",
-  "Chelsea FC", "CHE", "Chelsea",
-  "Crystal Palace FC", "CPA", "Crystal Palace",
-  "Everton FC", "EVE", "Everton",
-  "Fulham FC", "FUL", "Fulham",
-  "Leeds United", "LEE", "Leeds Utd",
-  "Liverpool FC", "LIV", "Liverpool",
-  "Manchester City FC", "MCI", "Man City",
-  "Manchester United FC", "MUN", "Man Utd",
-  "Newcastle United FC", "NEW", "Newcastle",
-  "Nottingham Forest FC", "NOT", "Notts Forest",
-  "Sunderland AFC", "SUN", "Sunderland",
-  "Tottenham Hotspur FC", "TOT", "Tottenham",
-  "West Ham United FC", "WHU", "West Ham",
-  "Wolverhampton Wanderers FC", "WOL", "Wolves"
+  ~teamName, ~shortName, ~midName, ~openName,
+  "Arsenal FC", "ARS", "Arsenal", "Arsenal FC",
+  "Aston Villa FC", "AST", "Aston Villa", "Aston Villa",
+  "AFC Bournemouth", "BOU", "Bournemouth", "AFC Bournemouth",
+  "Brentford FC", "BRE", "Brentford", "Brentford FC",
+  "Brighton & Hove Albion FC", "BRI", "Brighton", "Brighton & Hove Albion",
+  "Burnley FC", "BUR", "Burnley", "Burnley FC",
+  "Chelsea FC", "CHE", "Chelsea", "Chelsea FC",
+  "Crystal Palace FC", "CPA", "Crystal Palace", "Crystal Palace",
+  "Everton FC", "EVE", "Everton", "Everton FC",
+  "Fulham FC", "FUL", "Fulham", "Fulham FC",
+  "Leeds United", "LEE", "Leeds Utd", "Leeds United",
+  "Liverpool FC", "LIV", "Liverpool", "Liverpool FC",
+  "Manchester City FC", "MCI", "Man City", "Manchester City",
+  "Manchester United FC", "MUN", "Man Utd", "Manchester United",
+  "Newcastle United FC", "NEW", "Newcastle", "Newcastle United",
+  "Nottingham Forest FC", "NOT", "Notts Forest", "Nottingham Forest",
+  "Sunderland AFC", "SUN", "Sunderland", "Sunderland AFC",
+  "Tottenham Hotspur FC", "TOT", "Tottenham", "Tottenham Hotspur",
+  "West Ham United FC", "WHU", "West Ham", "West Ham United",
+  "Wolverhampton Wanderers FC", "WOL", "Wolves", "Wolverhampton Wanderers"
   )
 
 usethis::use_data(teams, overwrite = TRUE, internal = TRUE)
