9a5fc655fb9d4c88adb1af0c437bd4c9eeb5f920
p0bs <robin_penfold@yahoo.com>
2025-10-26 10:01:36 +0000

    Remove all but one test for now, and amend the other one

--- tests/testthat/test-data-previous_seasons.R
+++
@@ -1,5 +0,0 @@
-data("previous_seasons")
-
-test_that("correct size", {
-  expect_equal(ncol(previous_seasons), 10)
-})
--- tests/testthat/test-data-teams.R
+++ tests/testthat/test-data-teams.R
@@ -2,5 +2,5 @@ data(teams)
 
 test_that("correct size", {
   expect_equal(nrow(teams), 20)
-  expect_equal(ncol(teams), 3)
+  expect_equal(ncol(teams), 4)
 })
