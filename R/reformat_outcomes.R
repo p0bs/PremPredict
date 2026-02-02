4263c4c3a0526867348984cc813c4a3a3e63cbaf
p0bs <robin_penfold@yahoo.com>
2026-02-02 11:43:38 +0000

    Add reformat_outcomes function

---
+++ R/reformat_outcomes.R
@@ -0,0 +1,25 @@
+#' @title Reformat the outcomes data for improved presentation.
+#'
+#' @description This function takes the likelihoods of all possible standings for all clubs over this Premier League season and reformats them for improved presentation..
+#' @param value This is the outcome value to be reformatted.
+#' @keywords Sport
+#' @examples
+#' \dontrun{
+#' reformat_outcomes(
+#'   value = 0.94
+#'   )
+#' }
+
+reformat_outcomes <- function(value) {
+  dplyr::case_when(
+    dplyr::between(value, 0.001, 0.999) ~ paste0(
+      as.character(
+        round(100 * value, 1)
+      ),
+      "%"
+    ),
+    value < 0.001 ~ "< 0.1%",
+    value > 0.999 ~ "> 99.9%",
+    .default = "Error"
+  )
+}
---
+++ tests/testthat/test-reformat_outcomes.R
@@ -0,0 +1,16 @@
+test_that("functionality works", {
+  expect_equal(
+    reformat_outcomes(0.0002),
+    "< 0.1%"
+  )
+
+  expect_equal(
+    reformat_outcomes(0.99997),
+    "> 99.9%"
+  )
+
+  expect_equal(
+    reformat_outcomes(0.777777),
+    "77.8%"
+  )
+})
